fe6931bb10196ee3d2e4408f388ac9137e4f129b
Endg4me_ <65147948+Endg4meZer0@users.noreply.github.com>
2025-11-02 01:47:17 +1000

    fix(test): forgot to put the multiplier in the test

--- tests/output/server/event_marshal_test.go
+++ tests/output/server/event_marshal_test.go
@@ -23,7 +23,7 @@ func TestEventMarshal(t *testing.T) {
 		t.Error("[tests/output/server/TestEventMarshal] ERROR: " + err.Error())
 	}
 
-	expected := `{"Type":"ActiveLyricChanged","Data":{"Index":0,"Lyric":{"Timing":123.11,"Text":"yo~!"},"TimeUntilEnd":3.11,"Resync":false}}`
+	expected := `{"Type":"ActiveLyricChanged","Data":{"Index":0,"Lyric":{"Timing":123.11,"Text":"yo~!"},"Multiplier":0,"TimeUntilEnd":3.11,"Resync":false}}`
 	if string(out) != expected {
 		t.Errorf("[tests/output/server/TestEventMarshal] ERROR: wrong output, received %s instead of %s.", string(out), expected)
 	}
